7cf3656b0195f7196f2d663f82d72a8679994208
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-11 10:47:42 -0700

    card images layed out and wrapper content created. Had to create a .env file to bypass pre-flight check in order to test. need to ask about why error is being thrown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
-import Calculator from "./components/Calculator";
+import OfficeCards from "./components/Cards";
+import cards from "./cards.json";
 
-function App() {
-  return <Header />;
+class App extends React.Component {
+  state = {
+    cards
+  };
+
+  render() {
+    return (
+      <Wrapper>
+      {
+        this.state.cards.map(friend => (
+          <OfficeCards
+            id={friend.id}
+            name={friend.name}
+          />
+        ))
+      }
+      </Wrapper>
+    )
+  }
 }
-
 export default App;
