7e384f15f8f5497225601ec4962e1299e9c961f0
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-11 21:41:50 -0700

    styleing started for the cards

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function OfficeCards(props) {
     return (
         <div className="card" onClick={() => props.handleClick(props.id)}>
-            <span id={props.id} > <img alt={props.name} src={props.image} /> </span>
+            <span id={props.id} > <img className="image" alt={props.name} src={props.image} /> </span>
 
         </div>
 
