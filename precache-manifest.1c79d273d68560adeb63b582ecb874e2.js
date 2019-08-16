00d08d83403e38ce901b346be7fef9dce4f7d9a0
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-15 18:05:50 -0700

    Updates

---
+++ precache-manifest.1c79d273d68560adeb63b582ecb874e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca42d6dcf858b714356370661b3e30a",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "18adc17ce2927a022cbb",
+    "url": "/clickyGame/static/css/main.cbd47676.chunk.css"
+  },
+  {
+    "revision": "61477f81220081e1df57",
+    "url": "/clickyGame/static/js/2.901827b4.chunk.js"
+  },
+  {
+    "revision": "18adc17ce2927a022cbb",
+    "url": "/clickyGame/static/js/main.030ba2da.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
